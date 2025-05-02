9f4fa1ffd8c4271455920600ca1bf51cac9954f7
Sabari Kumar <sabarinkumar@gmail.com>
2025-05-02 08:17:41 -0600

    Precommit stuff

--- data/20250501_calc_mordred.py
+++ data/20250501_calc_mordred.py
@@ -1,23 +1,25 @@
-from rdkit import Chem
-from mordred import Calculator, descriptors
-import pandas as pd
-from typing import Union
 import os
+from typing import Union
+
+import pandas as pd
+from mordred import Calculator, descriptors
+from rdkit import Chem
+
 
 def calc_mordred(aqsol_csv: Union[str, os.PathLike]):
     aqsol_df = pd.read_csv(aqsol_csv)
-    mols = [Chem.AddHs(Chem.MolFromSmiles(x)) for x in aqsol_df['SMILES']]
+    mols = [Chem.AddHs(Chem.MolFromSmiles(x)) for x in aqsol_df["SMILES"]]
     calc = Calculator(descriptors, ignore_3D=True)
     mordred_df = calc.pandas(mols)
-    res_df = pd.concat([aqsol_df, mordred_df], axis = 1)
-    desc_df = res_df.select_dtypes(['number'])
-    desc_df['ID'] = mordred_df['ID']
-    desc_df = desc_df.set_index('ID')
+    res_df = pd.concat([aqsol_df, mordred_df], axis=1)
+    desc_df = res_df.select_dtypes(["number"])
+    desc_df["ID"] = mordred_df["ID"]
+    desc_df = desc_df.set_index("ID")
     desc_df = desc_df.dropna(axis=0)
-    desc_df.to_csv('../data/processed/20250501_aqsol_db_mordred_descs_cleaned.csv')
-    #res_df.to_csv('./processed/aqsol_db_mordred_descs.csv', index = False)
+    desc_df.to_csv("../data/processed/20250501_aqsol_db_mordred_descs_cleaned.csv")
+    # res_df.to_csv('./processed/aqsol_db_mordred_descs.csv', index = False)
     return res_df
 
-if __name__ == '__main__':
-    calc_mordred('./raw/20250502_AqSolDB_DatasetA.csv')
 
+if __name__ == "__main__":
+    calc_mordred("./raw/20250502_AqSolDB_DatasetA.csv")
